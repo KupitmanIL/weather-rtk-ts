beda3c611c5554aaae21e47b9e181ed652cb5284
edward <edwardlfish@gmail.com>
2026-03-30 21:08:23 +0300

    First commit

---
+++ src/features/api/weatherActions.ts
@@ -0,0 +1,22 @@
+import {api_key, base_url} from "../../utils/constants.ts";
+import {setWeather} from "../weather/weatherSlice.ts";
+import {setMessage} from "../message/messageSlice.ts";
+
+export const fetchWeather = city => async (dispatch) => {
+    try {
+        const res = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
+        const data = await res.json();
+        dispatch(setWeather({
+            country: data.sys.country,
+            city: data.name,
+            temp: data.main.temp,
+            pressure: data.main.pressure,
+            sunset: data.sys.sunset * 1000
+        }));
+        dispatch(setMessage(''));
+    } catch (e) {
+        console.log(e)
+        dispatch(setMessage('Enter correct city name'));
+        dispatch(setWeather({}));
+    }
+}
